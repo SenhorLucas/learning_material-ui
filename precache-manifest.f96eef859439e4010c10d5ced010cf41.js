518d91b3c748891a3f1382ccab79f5f326c07ed4
Lucas Viana <lucas.nviana@gmail.com>
2018-11-02 18:07:31 +0100

    Updates

---
+++ precache-manifest.f96eef859439e4010c10d5ced010cf41.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6137c24d7d2bd19d3f3",
+    "url": "/learning_material-ui/static/js/runtime~main.a6137c24.js"
+  },
+  {
+    "revision": "5b4867306ed5b52644c3",
+    "url": "/learning_material-ui/static/js/main.5b486730.chunk.js"
+  },
+  {
+    "revision": "d6c050801317e5f353ae",
+    "url": "/learning_material-ui/static/js/1.d6c05080.chunk.js"
+  },
+  {
+    "revision": "c64925718830b4fe9da2d5e5f6accc3c",
+    "url": "/learning_material-ui/index.html"
+  }
+];
